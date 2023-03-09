3a6bc1e54f98c44c88df7fea112b54db6559db0e
Buns <bunsthedev@gmail.com>
2023-03-09 02:44:02 -0600

    update: factory pragma

--- src/mocks/MockFactory.sol
+++ src/mocks/MockFactory.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 import '../lib/Libraries.sol';
-
-pragma solidity >=0.5.0;
+pragma solidity >=0.8.0;
 
 contract SoulSwapERC20 is ISoulSwapERC20 {
     using SafeMath for uint;
